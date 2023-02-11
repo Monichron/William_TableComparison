d79dde1c0e3d529beb5f0348c33d125978d1ea6e
user pc <user pc@DESKTOP-OJ9JERU>
2023-02-11 13:20:37 +0200

    Add project files.

---
+++ Comparison/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Comparison
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //connect to sql
+            SqlConnection Con = new SqlConnection();
+            Con.Open();
+            string strcmd = $"Select * From {ConfigurationManager}"
+            SqlCommand cmd = new SqlCommand();
+        }
+    }
+}
